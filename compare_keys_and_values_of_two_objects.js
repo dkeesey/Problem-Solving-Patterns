3214b4b8c8f4000dabe1a246244b4834cc6d2ff4
dkeesey <460435+dkeesey@users.noreply.github.com>
2023-01-09 16:06:29 -0800

    explaining why this works

--- compare_keys_and_values_of_two_objects.js
+++ compare_keys_and_values_of_two_objects.js
@@ -1,6 +1,10 @@
-let obj1 = {a: 1, b: 2};
-let obj2 = {a: 1, b: 2};
+let obj1 = { a: 1, b: 2 };
+let obj2 = { a: 1, b: 2 };
 
 let result = Object.entries(obj1).every(([key, value]) => obj2[key] === value);
-console.log(result);  
+console.log(result);
 
+// Object.entries returns an array of entries, and these are indexed in order;
+// if obj2 = {b:2, a:1}, reversed order, the indexes would be different, but the .every() 
+// comparison would still return true, because the comparison is of the Key values, 
+// which are still the same, even though they occur in different order.
